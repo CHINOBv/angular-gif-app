f454319f9e68c1f6cd8ac2f666978c0eda3a3c62
David Israel <retrochanel97@gmail.com>
2022-05-15 20:07:37 -0500

    Structure

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -3,6 +3,5 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.sass'],
 })
 export class SidebarComponent {}
